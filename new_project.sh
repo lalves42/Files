dab716e2e006182facc5b0c25eb21db44c00d740
LOIC_42 <LOIC_42@Mac-mini-de-LOIC-42.local>
2017-06-09 14:27:38 +0700

    Update newp now rm .git* after copying libft

--- new_project.sh
+++ new_project.sh
@@ -4,4 +4,4 @@ mkdir ~/Incoming/$1/src
 mkdir ~/Incoming/$1/inc
 cp -R ~/42Projects/Libft ~/Incoming/$1/libft
 cp ~/.Files/Makefile ~/.Files/auteur ~/Incoming/$1
-rm -rf ~/Incoming/$1/libft/.git
+rm -rf ~/Incoming/$1/libft/.git*
