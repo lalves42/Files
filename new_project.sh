4197370763e42b05018e91cf3c5f5527e1738f5f
LOIC_42 <LOIC_42@Mac-mini-de-LOIC-42.local>
2017-06-13 18:40:46 +0700

    update libft path on newp.sh

--- new_project.sh
+++ new_project.sh
@@ -2,6 +2,6 @@ echo Creating NewProject $1
 mkdir ~/Incoming/$1
 mkdir ~/Incoming/$1/src
 mkdir ~/Incoming/$1/inc
-cp -R ~/42Projects/Libft ~/Incoming/$1/libft
+cp -R ~/Libft ~/Incoming/$1/libft
 cp ~/.Files/Makefile ~/.Files/auteur ~/Incoming/$1
 rm -rf ~/Incoming/$1/libft/.git*
